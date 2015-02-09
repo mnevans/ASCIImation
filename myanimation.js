f3c9136ad0921eb773723034b68b5e9dda03a324
mnevans <evansmorg@gmail.com>
2015-02-08 17:52:38 -0800

    Create myanimation.js

---
+++ myanimation.js
@@ -0,0 +1,24 @@
+// Morgan Evans, CSE 154 AL, Homework 7
+// This page is my custom ASCIImation image of a cheerleader.
+
+var CHEERLEADER =
+"  _o/*\n" + 
+"*    \\\n" + 
+"      />\n" + 
+"=====\n" + 
+"  o__\n" + 
+"<\\ *\n" + 
+" */\\\n" + 
+"=====\n" + 
+"*\\o\n" + 
+"  />\n" + 
+" /\\*\n" + 
+"=====\n" + 
+" o/*\n" + 
+"<\\\n" + 
+" */\\\n" +
+"=====\n" + 
+"*\\o/*\n" + 
+"__)__\n";
+
+ANIMATIONS["Cheerleader"] = ANIMATIONS["cheerleader"] = ANIMATIONS["CHEERLEADER"] = CHEERLEADER;
